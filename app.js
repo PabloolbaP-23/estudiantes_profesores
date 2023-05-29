77f096ddc6485c7cde730ea4e40aa7678a4c9992
PabloolbaP-23 <pgarcsim2334@hotmail.com>
2023-05-29 10:20:42 +0200

    Ejercicio estudiantes_profesores

---
+++ app.js
@@ -0,0 +1,110 @@
+const formulario = document.querySelector("#formulario");
+const cardEstudiantes = document.querySelector("#cardEstudiantes");
+const cardProfesores = document.querySelector("#cardProfesores");
+const templateEstudiante = document.querySelector("#templateEstudiante").content;
+const templateProfesor = document.querySelector("#templateProfesor").content;
+const estudiantes = [];
+const profesores = [];
+
+document.addEventListener('click', e => {
+    if (e.target.dataset.nombre) {
+        if (e.target.matches(".btn-success")) {
+            estudiantes.map(item => {
+                if (item.nombre === e.target.dataset.nombre) {
+                    item.setEstado = true;
+                }
+                return item;
+            });
+        }
+    }
+    if (e.target.dataset.nombre) {
+        if (e.target.matches(".btn-danger")) {
+            estudiantes.map(item => {
+                if (item.nombre === e.target.dataset.nombre) {
+                    item.setEstado = false;
+                }
+                return item;
+            });
+        }
+    }
+    Persona.pintarPersonaUI(estudiantes, "Estudiante");
+});
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    static pintarPersonaUI(personas, tipo) {
+        if (tipo === "Estudiante") {
+            cardEstudiantes.textContent = "";
+            const fragment = document.createDocumentFragment();
+            personas.forEach(item => {
+                fragment.appendChild(item.agregarNuevoEstudiante());
+            });        
+            cardEstudiantes.appendChild(fragment);
+        } else {
+            cardProfesores.textContent = "";
+            const fragment = document.createDocumentFragment();
+            personas.forEach(item => {
+                fragment.appendChild(item.agregarNuevoProfesor());
+            }); 
+            cardProfesores.appendChild(fragment);
+        }
+    }
+}
+class Estudiante extends Persona{
+    #estado = false;
+    #estudiante = "Estudiante";
+    set setEstado(estado) {
+        this.#estado = estado;
+    }
+    get getEstudiante() {
+        return this.#estudiante;  
+    }
+
+    agregarNuevoEstudiante() {
+        const clone = templateEstudiante.cloneNode(true);
+        clone.querySelector('h5 .text-primary').textContent = this.nombre;
+        clone.querySelector('h6').textContent = this.getEstudiante;
+        clone.querySelector(".lead").textContent = this.edad;
+        if (this.#estado) {
+            clone.querySelector('.badge').className = "badge bg-success";
+            clone.querySelector(".btn-success").disabled = true;
+            clone.querySelector(".btn-danger").disabled = false;
+        } else {
+            clone.querySelector('.badge').className = "badge bg-danger";     
+            clone.querySelector(".btn-danger").disabled = true;
+            clone.querySelector(".btn-success").disabled = false;
+        }
+        clone.querySelector(".badge").textContent = this.#estado ? "Aprobado" : "Reprobado";
+        clone.querySelector('.btn-success').dataset.nombre = this.nombre
+        clone.querySelector('.btn-danger').dataset.nombre = this.nombre
+        return clone;       
+    }
+}
+class Profesor extends Persona{
+    #profesor = "Profesor"
+    agregarNuevoProfesor() {
+        const clone = templateProfesor.cloneNode(true);
+        clone.querySelector('h5').textContent = this.nombre;
+        clone.querySelector('h6').textContent = this.#profesor;
+        clone.querySelector('.lead').textContent = this.edad;
+        return clone;
+    }
+}
+
+formulario.addEventListener('submit', e =>{
+    e.preventDefault();
+    const datos = new FormData(formulario); // Par clave valor de los inputs
+    const [nombre, edad, opcion] = [...datos.values()];
+    if (opcion === "Estudiante") {
+        const estudiante = new Estudiante(nombre, edad);
+        estudiantes.push(estudiante); 
+        Persona.pintarPersonaUI(estudiantes, opcion);
+    } else { // Profesor
+        const profesor = new Profesor(nombre, edad);
+        profesores.push(profesor);
+        Persona.pintarPersonaUI(profesores, opcion);
+    }
+    
+});
